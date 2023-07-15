6130775ae3b88d5a52fcf5c29838634997e1b135
Vatsala Gupta <vatsala00000gupta@gmail.com>
2023-07-15 13:34:12 +0530

    done

--- src/components/OnePage.js
+++ src/components/OnePage.js
@@ -4,27 +4,27 @@ import './OnePage.css';
 const OnePage = () => {
 
     return (
-        <div>
-            <div>
+        <div className='main_div'>
+            <div className='secondary_div'>
                 <h5 className='h3'>Sneak Peak of Sanskriti College of Ayurvedic Medical Science</h5>
                 <h5 className='h31'>Medical College and Hospital Campus</h5>
             </div>
             <div class="container text-center">
-                <div className="row row-cols-3"style={{padding:"38px"}} >
-                    <div className="col"><img src='../Image/1.jpeg' style={{ width: "20rem" }}></img></div>
-                    <div className="col"><img src='./Image/2.jpeg' style={{ width: "20rem" }}></img></div>
-                    <div className="col"><img src='./Image/3.jpeg' style={{ width: "20rem" }}></img></div>
+                <div className="row_1 row-cols-3"style={{padding:"38px"}} >
+                    <div className="col1"><img src='../Image/1.jpeg' style={{ width: "20rem" }}></img></div>
+                    <div className="col1"><img src='./Image/2.jpeg' style={{ width: "20rem" }}></img></div>
+                    <div className="col1"><img src='./Image/3.jpeg' style={{ width: "20rem" }}></img></div>
 
                 </div>
 
-                <div className="row row-cols-3" style={{padding:"18px"}}>
-                    <div className="col"><img src='./Image/5.jpeg' style={{ width: "20rem" }}></img></div>
-                    <div className="col"><img src='./Image/6.jpeg' style={{ width: "20rem" }}></img></div>
-                    <div className="col"><img src='./Image/7.jpeg' style={{ width: "20rem" }}></img></div>
+                <div className="row_1 row-cols-3" style={{padding:"18px"}}>
+                    <div className="col1"><img src='./Image/5.jpeg' style={{ width: "20rem" }}></img></div>
+                    <div className="col1"><img src='./Image/6.jpeg' style={{ width: "20rem" }}></img></div>
+                    <div className="col1"><img src='./Image/7.jpeg' style={{ width: "20rem" }}></img></div>
 
                 </div>
-                <div className="row row-cols-1" style={{padding:"18px"}}>
-                    <div className="col"><img src='./Image/8.jpeg ' style={{width:"69rem"}}></img></div>
+                <div className="row_2 row-cols-1" style={{padding:"18px"}}>
+                    <div className="col2"><img  className='img' src='./Image/8.jpeg '/></div>
                 </div>
                 <div>
                     <h6 className='h4'>ONLY 17 SEATS LEFT!- <span className='spanyellow'> Without Donation</span></h6>
